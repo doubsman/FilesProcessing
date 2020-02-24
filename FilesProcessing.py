e7afc8f247a1d7d5484c7f05b1616374d70a4889
doubsman <doubsman@gmail.com>
2020-02-24 16:37:07 +0100

    1.0 lib for dbalbums

--- FilesProcessing.py
+++ FilesProcessing.py
@@ -1,10 +1,22 @@
 #! /usr/bin/python
 # coding: utf-8
 
+__author__ = "doubsman"
+__copyright__ = "Copyright 2020, Files Project"
+__credits__ = ["doubsman"]
+__license__ = "GPL"
+__version__ = "1.00"
+__maintainer__ = "doubsman"
+__email__ = "doubsman@doubsman.fr"
+__status__ = "Production"
+
+
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QObject, qDebug, QDateTime, QProcess
 from os import walk, rename, path, mkdir, remove, rmdir, startfile, listdir
 from sys import argv,platform
+from glob import glob
+from shutil import move, rmtree
 from codecs import open
 
 
@@ -18,12 +30,12 @@ class FilesProcessing(QObject):
 		self.blacklist = ['desktop.ini', 'Thumbs.db']
 		self.listfolders = None
 		self.listfiles = None
-		self.sizefoler = None
+		self.sizefolder = None
 
-	def folder_list_files(self, Cpath, boolsubfolders=True, masks=None, exact=None):
+	def folder_list_files(self, folderPath, boolsubfolders=True, masks=None, exact=None):
 		"""Build files list."""
 		self.listfiles = []
-		for folderName, subfolders, filenames in walk(Cpath):
+		for folderName, subfolders, filenames in walk(folderPath):
 			if subfolders and subfolders:
 				for subfolder in subfolders:
 					self.folder_list_files(subfolder, boolsubfolders, masks, exact)
@@ -48,49 +60,84 @@ class FilesProcessing(QObject):
 				break
 		return self.listfiles
 
-	def folder_list_folders(self, Cpath):
+	def folder_list_folders(self, folderPath):
 		"""Build folders list."""
-		self.listfolders = [d for d in listdir(Cpath) if path.isdir(path.join(Cpath, d))]
+		self.listfolders = [d for d in listdir(folderPath) if path.isdir(path.join(folderPath, d))]
 		return self.listfolders
 
-	def folder_size(self, Cpath,  boolsubfolders=True):
+	def folder_size(self, folderPath, boolsubfolders=True):
 		"""Calcul folder size."""
-		total_size = path.getsize(Cpath)
-		for item in listdir(Cpath):
-			itempath = path.join(Cpath, item)
+		total_size = path.getsize(folderPath)
+		for item in listdir(folderPath):
+			itempath = path.join(folderPath, item)
 			if path.isfile(itempath):
 				total_size += path.getsize(itempath)
 			elif path.isdir(itempath) and boolsubfolders:
 				total_size += self.folder_size(itempath)
+		self.sizefolder = total_size
 		return total_size
 
-	def open_folder(self, Cpath):
+	def folder_move(self, srcDir, dstDir):
+		"""Move files and folders command."""
+		# Check if both the are directories
+		if path.isdir(srcDir) and path.isdir(dstDir) :
+			# Iterate over all the files in source directory
+			for filePath in glob(srcDir.replace('[', '[[]') + '\*'): #, recursive=True):
+				# Move each file to destination Directory
+				move(filePath, dstDir)
+
+	def folder_delete(self, pathFolder):
+		"""Delete folder with the content."""
+		if path.exists(pathFolder):
+			rmtree(pathFolder)
+
+	def folder_delete_old(pathFolder):
+		"""Delete folder with the content."""
+		deleteFiles = []
+		deleteDirs = []
+		for root, dirs, files in walk(pathFolder):
+			for f in files:
+				deleteFiles.append(path.join(root, f))
+			for d in dirs:
+				deleteDirs.append(path.join(root, d))
+		for f in deleteFiles:
+			remove(f)
+		for d in deleteDirs:
+			rmdir(d)
+		rmdir(pathFolder)
+
+	def folder_open(self, folderPath):
 		"""Open File Explorer."""
 		if platform == "win32":
-			self.execute_command('explorer', Cpath)
+			self.execute_command('explorer', folderPath)
 		elif platform == "darwin":
-			self.execute_command('open', Cpath)
+			self.execute_command('open', folderPath)
 		elif platform == 'linux':
-			self.execute_command('xdg-open', Cpath)
+			self.execute_command('xdg-open', folderPath)
 
-	def open_file(self, file):
+	def file_open(self, filePath):
 		"""Open programme association with file."""
-		startfile(file)
+		startfile(filePath)
+
+	def file_delete(self, filePath):
+		"""File delete."""
+		if path.exists(filePath):
+			remove(filePath)
 
 	def execute_command(self, prog, *argv):
-		"""Execut a program no wait, no link."""
+		"""Execute a program no wait, no link."""
 		argums = []
 		for arg in argv:
 			argums += (arg,)
 		p = QProcess()
 		p.startDetached(prog, argums)
 
-	def convertUNC(self, Cpath):
+	def convertUNC(self, folderPath):
 		""" convert path UNC to linux."""
 		# open file unc from Linux (mount \HOMERSTATION\_lossLess)
 		if (platform == "darwin" or platform == 'linux') and path.startswith(r'\\'):
-			Cpath = r""+Cpath.replace('\\\\', '/').replace('\\', '/')
-		return Cpath
+			folderPath = r""+folderPath.replace('\\\\', '/').replace('\\', '/')
+		return folderPath
 
 
 if __name__ == '__main__':
@@ -102,15 +149,15 @@ if __name__ == '__main__':
 		# test envt
 		myfolder = r"D:\WorkDev\DBAlbumsTEST\TECHNO\Download"
 	# class
-	BuildProcess = FilesProcessing()
+	#BuildProcess = FilesProcessing()
 	#listfolders = BuildProcess.folder_list_folders(myfolder)
 	#variable CLASS BuildProcess.listfolders
 	#listfiles = BuildProcess.folder_list_files(myfolder, False)
 	#variable CLASS BuildProcess.listfiles
 	#listfiles = BuildProcess.folder_list_files(myfolder)
 	#foldersize = BuildProcess.folder_size(myfolder)
-	#BuildProcess.open_folder(myfolder)
-	#BuildProcess.open_file(r'D:\WorkDev\DBAlbumsTEST\TECHNO\Download\Nouveau document RTF.nfo')
+	#BuildProcess.folder_open(myfolder)
+	#BuildProcess.file_open(r'D:\WorkDev\DBAlbumsTEST\TECHNO\Download\Nouveau document RTF.nfo')
 	
 
 
